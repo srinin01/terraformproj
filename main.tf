8cd59ce5a260b69aae18cde0890e613088d6e876
Srini Personal <srinivasaraonalluri@Srinivasas-MacBook-Pro.local>
2021-10-31 12:58:02 -0400

    initial

--- main.tf
+++ main.tf
@@ -1,24 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
 
-  required_version = ">= 0.14.9"
-}
+  backend "remote" {
+-   organization = "REPLACE_ME"
++   organization = "SRINI_I"
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
+    workspaces {
+      name = "gh-actions-demo"
+    }
   }
 }
