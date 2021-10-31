5599c4113f99da67decfa0166bfaa5a86c3df8bc
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-10-31 17:12:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
 
-  required_version = ">= 0.14.9"
-}
+  backend "remote" {
+    organization = "test2322"
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
+    workspaces {
+      name = "gh-actions-demo"
+    }
   }
 }
